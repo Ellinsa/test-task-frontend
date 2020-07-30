f85bd25976fb100908c6ea4a771e505ec3c790d8
VikyLin <Airilet@yandex.ru>
2020-07-30 18:53:13 +0800

    auth form pop up - done

--- public/js/index.js
+++ public/js/index.js
@@ -21,3 +21,15 @@ function openCity(evt, tabName) {
 jQuery(document).ready(function () {
   jQuery(".wrapper-for-scroll").scrollbar();
 });
+
+//FORM
+
+// Get the modal
+var modal = document.getElementById("id01");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
